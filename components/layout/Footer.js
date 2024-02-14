a0695278c0947718ab72748426d61cf8c6a341b2
NobleFish <sirnoblefish@gmail.com>
2024-02-14 00:19:04 -0500

    Updated footer with disclaimer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      This is the footer content.
+      <a href='https://www.atlas-games.com/arsmagica'>Ars Magica</a> is a trademark of Atlas Games. All rights reserved. Learn more about the game at <a href='https://www.atlas-games.com/arsmagica'>atlas-games.com/arsmagica</a>.
     </footer>
   );
 };
