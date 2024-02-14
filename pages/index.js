fc5bde9b19790237d009b48f7c114a992ea7de28
NobleFish <sirnoblefish@gmail.com>
2024-02-14 00:21:04 -0500

    Updated text on home page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div>
       <div style={{ padding: '20px' }}>
-        <h1>Welcome to Ars Magica 5e</h1>
+        <h1> Ars Magica 5th Edition Helper</h1>
       </div>
     </div>
   );
